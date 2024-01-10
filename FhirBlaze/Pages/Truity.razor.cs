7853b801cbdf1920e891b3bf4a499a5f7c17e1e1
Dave Anderson <danderson@trulitehealth.com>
2024-01-10 10:33:35 -0700

    Added ability to embed provider app.

---
+++ FhirBlaze/Pages/Truity.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace FhirBlaze.Pages
+{
+    [Authorize]
+    public partial class Truity
+    {
+        [CascadingParameter] public Task<AuthenticationState> AuthTask { get; set; }
+
+    }
+}
